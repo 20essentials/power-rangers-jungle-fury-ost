188123cb2b5bd1c743b0dfa25b9414083b319b86
DominicDev <s0l0estudi4r@gmail.com>
2026-02-18 18:20:49 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'POWER RANGERS - JUNGLE FURY';
+export let nameSongs = [
+	"JUNGLE FURY THEME", /*1*/
+	"GO GO POWER RANGERS", /*2*/
+	"THEME INSTRUMENTAL", /*3*/
+	"FIGHT", /*4*/
+	"JUNGLE FURY THEME ON GUITAR", /*5*/
+	"HOPE FOR THE WORLD", /*6*/
+	"COMBAT", /*7*/
+	"CROSS MY LINE", /*8*/
+	"LORD ZEDD", /*9*/
+	"WE NEED A HERO", /*10*/
+	"UNITE", /*11*/
+	"5-4-1", /*12*/
+	"I WILL WIN", /*13*/
+	"YELLOW RANGER THEME", /*14*/
+	"MARCH OF THE DRAGON", /*15*/
+	"FORCE", /*16*/
+	"THEO", /*17*/
+	"KNIGHT", /*18*/
+	"NINJOR", /*19*/
+	"MMPR", /*20*/
+]
